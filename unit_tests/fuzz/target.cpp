8552d3bfcf147d097652d27a96586b82d5c28b1d
Adam Grandquist <grandquista@gmail.com>
2018-01-06 15:16:53 -0800

    Implement ProcessContext::parse_input.

--- unit_tests/fuzz/target.cpp
+++ unit_tests/fuzz/target.cpp
@@ -27,7 +27,7 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
   std::istringstream in(
       std::string(reinterpret_cast<const char *>(data), size));
   try {
-    module = processContext.parse_file(&in, "<fuzz>");
+    module = processContext.parse_file(in, "<fuzz>");
   } catch (const tao::pegtl::parse_error &) {
     success = false;
   }
