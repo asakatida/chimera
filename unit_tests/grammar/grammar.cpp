c6d1a06f1965124136ed46b49d25e8a4221422e3
Adam Grandquist <grandquista@gmail.com>
2018-01-31 17:03:05 -0800

    Add more tests.

---
+++ unit_tests/grammar/grammar.cpp
@@ -0,0 +1,16 @@
+#include "grammar/grammar.hpp"
+
+#include <catch.hpp>
+#include <tao/pegtl/analyze.hpp>
+
+TEST_CASE("grammar analyze (SingleInput)") {
+  REQUIRE(tao::pegtl::analyze<chimera::library::grammar::SingleInput>() == 0);
+}
+
+TEST_CASE("grammar analyze (FileInput)") {
+  REQUIRE(tao::pegtl::analyze<chimera::library::grammar::FileInput>() == 0);
+}
+
+TEST_CASE("grammar analyze (EvalInput)") {
+  REQUIRE(tao::pegtl::analyze<chimera::library::grammar::EvalInput>() == 0);
+}
