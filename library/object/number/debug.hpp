cd40f5792a4509052f747193e3215ce9bec0e931
Asa Katida <github@holomaplefeline.net>
2018-06-13 18:52:01 -0700

    Fix and operation bugs.

--- library/object/number/debug.hpp
+++ library/object/number/debug.hpp
@@ -32,7 +32,7 @@ namespace chimera {
 
           OStream &operator()(std::uint64_t i) { return os << i; }
 
-          OStream &operator()(const Base &base) { return os << base.value; }
+          OStream &operator()(Base base) { return os << base.value; }
 
           OStream &operator()(const Natural &natural) {
             std::for_each(natural.value.begin(), natural.value.end(), *this);
