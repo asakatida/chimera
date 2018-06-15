5c354ff3eb285d72beec136f05115018e7b80433
Asa Katida <github@holomaplefeline.net>
2018-06-15 16:14:10 -0700

    Split module tooling.

---
+++ tools/modules.cpp
@@ -0,0 +1,108 @@
+// Copyright (c) 2018 Asa Katida <github@holomaplefeline.net>
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+//! evaluates importlib to construct the importlib module.
+//! Then prints the module construction.
+
+#include "modules.hpp"
+
+#include <algorithm>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <optional>
+#include <queue>
+#include <string>
+#include <vector>
+
+#include <gsl/gsl>
+
+#include "asdl/asdl.hpp"
+#include "object/object.hpp"
+#include "options.hpp"
+#include "virtual_machine/virtual_machine.hpp"
+
+namespace chimera {
+  namespace library {
+    Printer::Printer(object::Object object, std::string name) : main(object) {
+      _printed.try_emplace(id(main), name);
+    }
+
+    std::string Printer::printed(const object::Object &object) {
+      return _printed.at(id(object));
+    }
+
+    object::Id Printer::id(const object::Object &object) {
+      return _remap.try_emplace(object.id(), object.id()).first->second;
+    }
+
+    void Printer::remap(const object::Object &module,
+                      const object::Object &previous) {
+      if (!_remap.try_emplace(previous.id(), module.id()).second) {
+        return;
+      }
+      for (const auto &name : module.dir()) {
+        if (previous.has_attribute(name)) {
+          remap(module.get_attribute(name), previous.get_attribute(name));
+        }
+      }
+      for (const auto &name : previous.dir()) {
+        if (module.has_attribute(name)) {
+          remap(module.get_attribute(name), previous.get_attribute(name));
+        }
+      }
+    }
+
+    bool Printer::is_printed(const object::Object &object) {
+      return _printed.count(id(object)) != 0;
+    }
+
+    bool Compare::operator()(const SetAttribute &a,
+                             const SetAttribute &b) const {
+      if (a.base_name == b.base_name) {
+        return a.name < b.name;
+      }
+      return a.base_name < b.base_name;
+    }
+
+    bool Compare::operator()(const Work &a, const Work &b) const {
+      if (a.printer->is_printed(b.object) && !b.printer->is_printed(a.object)) {
+        return true;
+      }
+      if (b.printer->is_printed(a.object) && !a.printer->is_printed(b.object)) {
+        return false;
+      }
+      if (a.base_name == b.base_name) {
+        return a.name > b.name;
+      }
+      return a.base_name > b.base_name;
+    }
+
+    std::optional<object::Object>
+    IncompleteTuple::operator()(const object::Tuple &tuple) const {
+      for (const auto &object : tuple) {
+        if (!printer->is_printed(object)) {
+          return {object};
+        }
+      }
+      return {};
+    }
+  } // namespace library
+} // namespace chimera
---
+++ tools/modules.hpp
@@ -0,0 +1,323 @@
+// Copyright (c) 2018 Asa Katida <github@holomaplefeline.net>
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+//! evaluates importlib to construct the importlib module.
+//! Then prints the module construction.
+
+#include <algorithm>
+#include <iomanip>
+#include <map>
+#include <optional>
+#include <queue>
+#include <string>
+#include <vector>
+
+#include <gsl/gsl>
+
+#include "asdl/asdl.hpp"
+#include "object/object.hpp"
+#include "options.hpp"
+#include "virtual_machine/virtual_machine.hpp"
+
+namespace chimera {
+  namespace library {
+    struct Printer;
+
+    struct SetAttribute {
+      std::string base_name;
+      std::string name;
+    };
+
+    struct Work {
+      Printer *printer;
+      object::Object object;
+      std::string base_name;
+      std::string name;
+    };
+
+    struct Compare {
+      bool operator()(const SetAttribute &a, const SetAttribute &b) const;
+      bool operator()(const Work &a, const Work &b) const;
+    };
+
+    struct IncompleteTuple {
+      Printer *printer;
+
+      std::optional<object::Object>
+      operator()(const object::Tuple &tuple) const;
+      template <typename Type>
+      std::optional<object::Object> operator()(const Type & /*type*/) const {
+        return {};
+      }
+    };
+
+    struct Printer {
+      Printer(object::Object object, std::string name);
+
+      std::string printed(const object::Object &object);
+
+      object::Id id(const object::Object &object);
+
+      void remap(const object::Object &module, const object::Object &previous);
+
+      bool is_printed(const object::Object &object);
+
+      template <typename OStream>
+      OStream &print(OStream &os, const object::Instance & /*instance*/) {
+        return os << "object::Instance{}";
+      }
+
+      template <typename OStream>
+      OStream &print(OStream &os, const object::Bytes &bytes) {
+        os << "object::Bytes{";
+        bool first = true;
+        for (const auto &byte : bytes) {
+          if (!first) {
+            os << ",";
+          } else {
+            first = false;
+          }
+          os << byte;
+        }
+        return os << "}";
+      }
+
+      template <typename OStream>
+      OStream &print(OStream &os, const object::BytesMethod &bytesMethod) {
+        os << "object::BytesMethod::";
+        switch (bytesMethod) {}
+        return os;
+      }
+
+      template <typename OStream>
+      OStream &print(OStream &os, const object::False & /*false*/) {
+        return os << "object::False{}";
+      }
+
+      template <typename OStream>
+      OStream &print(OStream &os, const object::Future & /*future*/) {
+        Expects(false);
+        return os;
+      }
+
+      template <typename OStream>
+      OStream &print(OStream &os, const object::None & /*none*/) {
+        return os << "object::None{}";
+      }
+
+      template <typename OStream>
+      OStream &print(OStream &os,
+                          const object::NullFunction & /*nullFunction*/) {
+        return os << "object::NullFunction{}";
+      }
+
+      template <typename OStream>
+      OStream &print(OStream &os, const object::Number &number) {
+        return number.repr(os);
+      }
+
+      template <typename OStream>
+      OStream &print(OStream &os, const object::NumberMethod &numberMethod) {
+        os << "object::NumberMethod::";
+        switch (numberMethod) {}
+        return os;
+      }
+
+      template <typename OStream>
+      OStream &print(OStream &os, const object::ObjectMethod &objectMethod) {
+        os << "object::ObjectMethod::";
+        switch (objectMethod) {
+          case object::ObjectMethod::DELATTR:
+            return os << "DELATTR";
+          case object::ObjectMethod::DIR:
+            return os << "DIR";
+          case object::ObjectMethod::GETATTRIBUTE:
+            return os << "GETATTRIBUTE";
+          case object::ObjectMethod::SETATTR:
+            return os << "SETATTR";
+        }
+        return os;
+      }
+
+      template <typename OStream>
+      OStream &print(OStream &os, const object::String &string) {
+        return os << "object::String(" << std::quoted(string.value) << "s)";
+      }
+
+      template <typename OStream>
+      OStream &print(OStream &os, const object::StringMethod &stringMethod) {
+        os << "object::StringMethod::";
+        switch (stringMethod) {}
+        return os;
+      }
+
+      template <typename OStream>
+      OStream &print(OStream &os, const object::True & /*true*/) {
+        return os << "object::True{}";
+      }
+
+      template <typename OStream>
+      OStream &print(OStream &os, const object::Tuple &tuple) {
+        os << "object::Tuple{";
+        bool first = true;
+        for (const auto &object : tuple) {
+          if (!first) {
+            os << ",";
+          } else {
+            first = false;
+          }
+          os << printed(object);
+        }
+        return os << "}";
+      }
+
+      template <typename OStream>
+      OStream &print(OStream &os, const object::TupleMethod &tupleMethod) {
+        os << "object::TupleMethod::";
+        switch (tupleMethod) {}
+        return os;
+      }
+
+      template <typename OStream>
+      OStream &print(OStream &os, const object::SysCall &sysCall) {
+        os << "object::SysCall::";
+        switch (sysCall) {
+          case object::SysCall::COMPILE:
+            return os << "COMPILE";
+          case object::SysCall::EVAL:
+            return os << "EVAL";
+          case object::SysCall::EXEC:
+            return os << "EXEC";
+          case object::SysCall::GLOBALS:
+            return os << "GLOBALS";
+          case object::SysCall::ID:
+            return os << "ID";
+          case object::SysCall::INPUT:
+            return os << "INPUT";
+          case object::SysCall::LOCALS:
+            return os << "LOCALS";
+          case object::SysCall::PRINT:
+            return os << "PRINT";
+          case object::SysCall::OPEN:
+            return os << "OPEN";
+        }
+        return os;
+      }
+
+      template <typename OStream>
+      OStream &print(OStream &os, const asdl::StmtImpl &stmtImpl) {
+        return os << &stmtImpl;
+      }
+
+      template <typename OStream>
+      OStream &print(OStream &os, const asdl::ExprImpl &exprImpl) {
+        return os << &exprImpl;
+      }
+
+      template <typename OStream>
+      OStream &print(OStream &os, const Work &work) {
+        auto baseName = std::string(work.base_name).append("_").append(work.name);
+        auto object = std::visit(IncompleteTuple{this}, work.object.value());
+        while (object) {
+          print(os, Work{this, *object, baseName, std::to_string(_printed.size())});
+          object = std::visit(IncompleteTuple{this}, work.object.value());
+        }
+        if (is_printed(work.object)) {
+          return os;
+        }
+        os << "object::Object " << baseName << "(";
+        std::visit([this, &os](const auto &value) { this->print(os, value); },
+                   work.object.value());
+        os << ",{";
+        bool first = true;
+        for (const auto &name : work.object.dir()) {
+          if (!first) {
+            os << ",";
+          } else {
+            first = false;
+          }
+          os << "{" << std::quoted(name) << "s,";
+          if (is_printed(work.object.get_attribute(name))) {
+            os << printed(work.object.get_attribute(name));
+          } else {
+            os << "{/*set below*/}";
+            wanted[id(work.object.get_attribute(name))].push_back(
+                SetAttribute{baseName, name});
+            queue.push(
+                Work{this, work.object.get_attribute(name), baseName, name});
+          }
+          os << "}";
+        }
+        _printed.try_emplace(id(work.object), baseName);
+        os << "});";
+        if (wanted.count(id(work.object)) != 0) {
+          auto setAttributes = std::move(wanted.at(id(work.object)));
+          wanted.erase(id(work.object));
+          std::sort(setAttributes.begin(), setAttributes.end(), Compare{});
+          for (const auto &setAttribute : setAttributes) {
+            os << setAttribute.base_name << ".set_attribute("
+               << std::quoted(setAttribute.name) << "s,"
+               << printed(work.object) << ");";
+          }
+        }
+        return os;
+      }
+
+      std::map<object::Id, object::Id> _remap;
+      std::map<object::Id, std::string> _printed;
+      std::priority_queue<Work, std::vector<Work>, Compare> queue;
+      std::map<object::Id, std::vector<SetAttribute>> wanted;
+      object::Object main;
+    };
+
+    template <typename OStream>
+    OStream &operator<<(OStream &os, Printer &printer) {
+      auto module_name = printer.printed(printer.main);
+      os
+          << "//! generated file see tools/" << module_name << ".cpp\n\n"
+             "#include \"virtual_machine/modules/" << module_name << ".hpp\"\n\n"
+             "#include \"object/object.hpp\"\n\n"
+             "using namespace std::literals;\n\n"
+             "namespace chimera {\nnamespace library {\n"
+             "namespace virtual_machine {\nnamespace modules {\n"
+             "void " << module_name << "(const object::Object &module) {auto "
+          << module_name
+          << " = module;";
+      for (const auto &name : printer.main.dir()) {
+        if (printer.is_printed(printer.main.get_attribute(name))) {
+          os << module_name << ".set_attribute(" << std::quoted(name) << "s,"
+                    << printer.printed(printer.main.get_attribute(name))
+                    << ");";
+        } else {
+          printer.wanted[printer.id(printer.main.get_attribute(name))]
+              .push_back(SetAttribute{module_name, name});
+          printer.queue.push(
+              Work{&printer, printer.main.get_attribute(name), module_name, name});
+        }
+      }
+      while (!printer.queue.empty()) {
+        auto work = printer.queue.top();
+        printer.queue.pop();
+        printer.print(os, work);
+      }
+      return os << "}\n}\n}\n}\n}" << std::endl;
+    }
+  } // namespace library
+} // namespace chimera
