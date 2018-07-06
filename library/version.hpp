c3f43845b733c43f96d80d394cad6f9e197b74fd
Adam Grandquist <grandquista@gmail.com>
2018-07-06 14:43:18 -0700

    Finalize complex conversion.

--- library/version.hpp
+++ library/version.hpp
@@ -4,3 +4,9 @@
 
 #define CHIMERA_VERSION "v1.0.0a0"
 #define CHIMERA_GIT_HEAD "v1.0.0a0"
+
+namespace chimera {
+  namespace library {
+    void version();
+  } // namespace library
+} // namespace chimera
