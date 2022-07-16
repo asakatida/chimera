d2038a9fab6eb1ad7585e22b8d3a374ade849c79
Asa Katida <github@holomaplefeline.net>
2022-07-16 12:29:55 -0700

    Finalize current clang-tidy layout.

--- tools/clang-tidy.sh
+++ tools/clang-tidy.sh
@@ -5,5 +5,6 @@ set -ex -o pipefail
 root="$(git rev-parse --show-toplevel)"
 
 find "${root}" -name '*.cpp' -print0 | \
-  xargs -0 -- git ls-tree --full-tree --name-only -z HEAD -- | \
-  xargs -0 -- clang-tidy -p="${root}" -quiet -fix -fix-errors -fix-notes
+  "${root}/tools/g-ls-tree.sh" | \
+  xargs -0 -- clang-tidy -p="$1" -quiet -fix -fix-errors -fix-notes \
+  >"$1/clang-tidy.log"
