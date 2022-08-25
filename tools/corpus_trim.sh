5b5bd3cb2b8fd16e4fecbd1f43a3fceeff322a7d
Asa Katida <github@holomaplefeline.net>
2022-08-25 09:49:16 -0700

    Add a crash reproduction.

--- tools/corpus_trim.sh
+++ tools/corpus_trim.sh
@@ -4,13 +4,31 @@ set -ex -o pipefail
 
 root="$(git rev-parse --show-toplevel)"
 
+if [[ -z "$(find "${root}" -name fuzz-vm -perm -0110 -type f || true)" ]]; then
+  exit
+fi
+
+find "${root}/build/" \
+  '(' -name 'crash-*' -or -name 'leak-*' -or -name 'timeout-*' ')' \
+  -type f -exec mv {} "${root}/unit_tests/fuzz/crashes" ';'
+pushd "${root}/unit_tests/fuzz/crashes"
+# shellcheck disable=SC2016
+find . -name '*-*' -type f -print0 | \
+  xargs --no-run-if-empty --null -- \
+  bash -c 'mv "$1" "./${1/.*-/}"' --
+popd
+
 mv "${root}/unit_tests/fuzz/corpus" "${root}/unit_tests/fuzz/corpus_original"
 mkdir "${root}/unit_tests/fuzz/corpus"
 
-find "${root}/build/" -name 'fuzz-*' -perm -0110 -type f \
-  -exec '{}' -merge=1 \
+find "${root}" -name fuzz-vm -perm -0110 -type f -print0 | \
+  xargs --no-run-if-empty --null --replace='{}' --verbose -- env \
+  '{}' -merge=1 \
+  -reduce_inputs=1 \
+  -shrink=1 \
+  -use_value_profile=1 \
   "${root}/unit_tests/fuzz/corpus" \
-  "${root}/unit_tests/fuzz/corpus_original" ';'
+  "${root}/unit_tests/fuzz/corpus_original"
 
 rm -rf "${root}/unit_tests/fuzz/corpus_original"
 
