6e8aa77b8a4d31316c11348ef8c60b1cd504c4fa
asakatida <asakatida@users.noreply.github.com>
2022-09-08 01:25:07 +0000

    Update requirements.txt.

--- tools/venv.sh
+++ tools/venv.sh
@@ -7,7 +7,6 @@ cd "$(git rev-parse --show-toplevel || true)"
 rm -rf env
 "$1" -m venv env
 export PATH="${PWD}/env/bin:${PATH}"
-pip3=env/bin/pip3
-"${pip3}" install --upgrade pip setuptools wheel
-"${pip3}" install -r requirements.txt
-"${pip3}" freeze | grep -v -e '^pkg_resources==' >requirements.txt
+pip3 install --upgrade pip setuptools wheel
+pip3 install -r requirements.txt
+pip3 freeze | grep -v -e '^pkg_resources==' >requirements.txt
