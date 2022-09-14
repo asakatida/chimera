de4b9dba318d124c30c3a45cf9d75d4bad8bc823
Asa Katida <github@holomaplefeline.net>
2022-09-14 20:26:44 +0000

    Fix trim handling for bytes data and corpus.

--- tools/trim.py
+++ tools/trim.py
@@ -3,41 +3,48 @@ from pathlib import Path
 from re import MULTILINE, Match, compile
 from sys import argv
 
+ROOT = Path(__file__).resolve().parent.parent
+# patches need to stay as is
+# fuzz corpus is ignored for utf8 nonsense
+IGNORE = (
+    (ROOT / "patches"),
+    (ROOT / "unit_tests" / "fuzz" / "corpus"),
+    (ROOT / "unit_tests" / "fuzz" / "crashes"),
+)
 SEARCHES = (
     compile(
-        r"$\s^$(?P<keep>\s{2,})(?=\S)(?!private:)", MULTILINE
+        rb"$\s^$(?P<keep>\s{2,})(?=\S)(?!private:)", MULTILINE
     ),  # no runs of blank lines
-    compile(r"((?=[^\n])\s)+(?P<keep>$)", MULTILINE),  # no line ending space
-    compile(r"^(?P<keep>( +\t *| *\t +))", MULTILINE),  # no mixing tabs with spaces
+    compile(rb"((?=[^\n])\s)+(?P<keep>$)", MULTILINE),  # no line ending space
+    compile(rb"^(?P<keep>( +\t *| *\t +))", MULTILINE),  # no mixing tabs with spaces
 )
 
 
-def sub(match: Match[str]) -> str:
+def sub(match: Match[bytes]) -> bytes:
     keep = match.group("keep")
-    if "\t" in keep and "\n" not in keep:
-        return compile(r" {2}").sub("\t", keep)
+    if b"\t" in keep and b"\n" not in keep:
+        return compile(rb" {2}").sub(b"\t", keep)
     return keep
 
 
 def test(f: Path) -> bool:
     # markdown is ignored
-    # patches need to stay
-    # python linting is covered elsewhere
+    # python linting is covered by black
     return (
-        f.suffix not in (".md", ".py")
-        and "patches" not in f.parts
-        and any(map(lambda search: search.search(f.read_text()), SEARCHES))
+        (not any(map(f.resolve().is_relative_to, IGNORE)))
+        and f.suffix not in (".md", ".py")
+        and any(map(lambda search: search.search(f.read_bytes()), SEARCHES))
     )
 
 
 files = list(filter(test, filter(Path.is_file, map(Path, argv[1:]))))
 while files:
     for file in files:
-        file.write_text(
+        file.write_bytes(
             reduce(
                 lambda content, search: search.sub(sub, content),
                 SEARCHES,
-                file.read_text(),
+                file.read_bytes(),
             )
         )
     files = list(filter(test, files))
