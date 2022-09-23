4f37a53f03bca0bc15accfe7fcc6d3e9357cdfff
Asa Katida <github@holomaplefeline.net>
2022-09-23 20:36:16 +0000

    Update fuzzing corpus.

--- tools/regression.sh
+++ tools/regression.sh
@@ -3,16 +3,14 @@
 set -e -o pipefail
 
 case $# in
-  0 )
-    echo "Usage: $0 <fuzzer> <corpus-dir>" >&2
-    exit 1
-    ;;
   2 )
     find "$2" -type f -print0 | \
       xargs --no-run-if-empty --null -- \
-      "$0" "$1"
+      "$1" -detect_leaks=0 -use_value_profile=1 -max_total_time=500 \
+      >"/tmp/regression-$(basename "$1").logs" 2>&1
     ;;
   * )
-    "$@" >>"/tmp/regression-$(basename "$1").logs" 2>&1
+    echo "Usage: $0 <fuzzer> <corpus-dir>" >&2
+    exit 1
     ;;
 esac
