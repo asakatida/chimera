dcdddf5d458d39d0ec80e99f60ecd04be2657cc8
Asa Katida <github@holomaplefeline.net>
2022-10-13 18:19:33 -0700

    Fix chimera::library::virtual_machine::SliceEvaluator scope.

--- library/virtual_machine/slice_evaluator.cpp
+++ library/virtual_machine/slice_evaluator.cpp
@@ -25,6 +25,8 @@
 #include "virtual_machine/evaluator.hpp"
 
 namespace chimera::library::virtual_machine {
+  SliceEvaluator::SliceEvaluator(Evaluator *evaluator) noexcept
+      : evaluator(evaluator) {}
   void SliceEvaluator::operator()(const asdl::Slice & /*slice*/) const {
     evaluator->push(PushStack{evaluator->builtins().get_attribute("None")});
   }
