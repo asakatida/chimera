062f9f3e9d018b7a75912d92aa92a633715c1633
Asa Katida <github@holomaplefeline.net>
2022-10-29 14:08:27 -0700

    Adjust timing default values.

--- tools/asyncio_cmd.py
+++ tools/asyncio_cmd.py
@@ -18,8 +18,8 @@ class ProcessError(Exception):
 
 
 async def cmd(
-    *args: str, log: bool = True, stdout: Optional[int] = DEVNULL, timeout: int = 10
-) -> Optional[bytes]:
+    *args: str, log: bool = True, stdout: Optional[int] = DEVNULL, timeout: int = 20
+) -> bytes:
     if log:
         print("+", *args, file=stderr)
     proc = await create_subprocess_exec(*args, stderr=PIPE, stdout=stdout)
@@ -30,11 +30,11 @@ async def cmd(
         if proc.returncode is None:
             proc.terminate()
             await proc.wait()
-            cmd_stderr += (
-                f"\nThe process was terminated by timeout {timeout} seconds\n".encode()
-            )
             if proc.stderr is not None:
                 cmd_stderr += await proc.stderr.read()
+            cmd_stderr += (
+                f"\nThe process was terminated by timeout {timeout} seconds".encode()
+            )
         if proc.returncode != 0:
             raise ProcessError(args, cmd_stderr, proc.returncode or 1)
     return cmd_stdout
