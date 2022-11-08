8d8ed9b5dcce6087d9c9b70604e2a2e04b502570
Asa Katida <github@holomaplefeline.net>
2022-11-07 21:39:51 -0800

    Untangle fuzzing from unit-tests builds.

--- tools/ninja.sh
+++ tools/ninja.sh
@@ -3,6 +3,8 @@
 set -e -o pipefail
 
 ninja -C "$1" -j1 chimera-grammar
-ninja -C "$1" -j2 chimera unit-test
+ninja -C "$1" -j2 chimera fuzzers
+ninja -C "$1" Catch2
+ninja -C "$1" -j2 unit-test
 ninja -C "$1"
 ninja -C "$@"
