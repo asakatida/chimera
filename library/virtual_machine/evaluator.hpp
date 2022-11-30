15e6144fedd96524d0d0e7785f440522df22c1da
Asa Katida <github@holomaplefeline.net>
2022-11-29 17:07:51 -0800

    Sort method declarations in evaluator.

--- library/virtual_machine/evaluator.hpp
+++ library/virtual_machine/evaluator.hpp
@@ -89,67 +89,68 @@ namespace chimera::library::virtual_machine {
   };
   struct Evaluator {
     explicit Evaluator(ThreadContext &thread_context) noexcept;
-    auto self() -> object::Object &;
     [[nodiscard]] auto builtins() const -> const object::Object &;
     void enter_scope(const object::Object &object);
     void enter();
-    void exit();
     void exit_scope();
-    void extend(const std::vector<asdl::StmtImpl> &instructions);
+    void exit();
     void extend(const std::vector<asdl::ExprImpl> &instructions);
+    void extend(const std::vector<asdl::StmtImpl> &instructions);
+    void get_attribute(const object::Object &object, const std::string &name);
     template <typename Instruction>
     void push(Instruction &&instruction) {
       scope.push(std::forward<Instruction>(instruction));
     }
     [[nodiscard]] auto return_value() const -> object::Object;
+    auto self() -> object::Object &;
     void stack_pop();
     void stack_push(const object::Object &object);
     [[nodiscard]] auto stack_remove() -> object::Object;
     [[nodiscard]] auto stack_size() const -> std::size_t;
     [[nodiscard]] auto stack_top() const -> const object::Object &;
     void stack_top_update(const object::Object &object);
-    void evaluate(const asdl::StmtImpl &stmt);
+    // Evaluators
     void evaluate_del(const asdl::ExprImpl &expr);
     void evaluate_get(const asdl::ExprImpl &expr);
     void evaluate_set(const asdl::ExprImpl &expr);
-    void get_attribute(const object::Object &object, const std::string &name);
     void evaluate();
-    void evaluate(const asdl::Module &module);
-    void evaluate(const asdl::Interactive &interactive);
-    void evaluate(const asdl::Expression &expression);
-    void evaluate(const asdl::FunctionDef &functionDef);
-    void evaluate(const asdl::AsyncFunctionDef &asyncFunctionDef);
-    void evaluate(const asdl::ClassDef &classDef);
-    void evaluate(const asdl::Delete &asdlDelete);
-    void evaluate(const asdl::Assign &assign);
-    void evaluate(const asdl::AugAssign &augAssign);
     void evaluate(const asdl::AnnAssign &annAssign);
-    void evaluate(const asdl::For &asdlFor);
+    void evaluate(const asdl::Assert &assert);
+    void evaluate(const asdl::Assign &assign);
     void evaluate(const asdl::AsyncFor &asyncFor);
-    void evaluate(const asdl::While &asdlWhile);
-    void evaluate(const asdl::If &asdlIf);
-    void evaluate(const asdl::With &with);
+    void evaluate(const asdl::AsyncFunctionDef &asyncFunctionDef);
     void evaluate(const asdl::AsyncWith &asyncWith);
+    void evaluate(const asdl::AugAssign &augAssign);
+    void evaluate(const asdl::Break &asdlBreak);
+    void evaluate(const asdl::ClassDef &classDef);
+    void evaluate(const asdl::Continue &asdlContinue);
+    void evaluate(const asdl::Delete &asdlDelete);
+    void evaluate(const asdl::Expr &expr);
+    void evaluate(const asdl::Expression &expression);
+    void evaluate(const asdl::For &asdlFor);
+    void evaluate(const asdl::FunctionDef &functionDef);
+    void evaluate(const asdl::Global &global);
+    void evaluate(const asdl::If &asdlIf);
     void evaluate(const asdl::Import &import);
     void evaluate(const asdl::ImportFrom &importFrom);
-    void evaluate(const asdl::Global &global);
+    void evaluate(const asdl::Interactive &interactive);
+    void evaluate(const asdl::Module &module);
     void evaluate(const asdl::Nonlocal &nonlocal);
-    void evaluate(const asdl::Expr &expr);
     void evaluate(const asdl::Raise &raise);
-    void evaluate(const asdl::Try &asdlTry);
-    void evaluate(const asdl::Assert &assert);
     void evaluate(const asdl::Return &asdlReturn);
-    void evaluate(const asdl::Break &asdlBreak);
-    void evaluate(const asdl::Continue &asdlContinue);
+    void evaluate(const asdl::StmtImpl &stmt);
+    void evaluate(const asdl::Try &asdlTry);
+    void evaluate(const asdl::While &asdlWhile);
+    void evaluate(const asdl::With &with);
 
   private:
     auto do_try(const std::vector<asdl::StmtImpl> &body,
                 const std::optional<object::BaseException> &context)
         -> std::optional<object::BaseException>;
+    void get_attr(const object::Object &object, const std::string &name);
     void get_attribute(const object::Object &object,
                        const object::Object &getAttribute,
                        const std::string &name);
-    void get_attr(const object::Object &object, const std::string &name);
     ThreadContext &thread_context;
     Scopes scope{};
     std::stack<object::Object> stack{};
