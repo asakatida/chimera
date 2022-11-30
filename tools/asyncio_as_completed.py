0431906b5bad6f18b02714ebdd01a7ca68d88a5b
Asa Katida <github@holomaplefeline.net>
2022-11-29 16:24:58 -0800

    Tune as_completed function queues.

--- tools/asyncio_as_completed.py
+++ tools/asyncio_as_completed.py
@@ -1,12 +1,16 @@
-from asyncio import Queue, create_task, wait_for
+from asyncio import Queue, Task, create_task, gather, wait_for
 from os import cpu_count
 from typing import AsyncGenerator, Coroutine, Iterable, TypeVar
 
 T = TypeVar("T")
 
 
-async def worker(queue: Queue[Coroutine[object, object, T]], output: Queue[T]) -> None:
-    while True:
+async def worker(
+    task: Task[None],
+    queue: Queue[Coroutine[object, object, T]],
+    output: Queue[T],
+) -> None:
+    while not (task.done() and queue.empty()):
         coroutine = await queue.get()
         await output.put(await coroutine)
         queue.task_done()
@@ -25,16 +29,14 @@ async def as_completed(
     coroutines: Iterable[Coroutine[object, object, T]],
     limit: int = max(12, (cpu_count() or 1) // 4),
 ) -> AsyncGenerator[T, object]:
-    queue: Queue[Coroutine[object, object, T]] = Queue(limit)
-    output: Queue[T] = Queue(limit)
-    workers = [create_task(worker(queue, output)) for _ in range(limit)]
+    queue: Queue[Coroutine[object, object, T]] = Queue(1)
+    output: Queue[T] = Queue()
     task = create_task(producer(coroutines, queue))
-    while True:
+    workers = [create_task(worker(task, queue, output)) for _ in range(limit)]
+    while not (task.done() and output.empty()):
         try:
-            yield await wait_for(output.get(), 2)
+            yield await wait_for(output.get(), 0.5)
             output.task_done()
         except TimeoutError:
-            if task.done():
-                break
-    await wait_for(task, 2)
-    [worker.cancel() for worker in workers]
+            pass
+    gather(task, *workers, return_exceptions=True)
