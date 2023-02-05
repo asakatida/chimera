f0af2f416df200f5b4ad6f1fd4f4c0390db65a02
Asa Katida <github@holomaplefeline.net>
2023-02-04 18:15:17 -0800

    Separate out the utils for cleanup of fuzzer corpus.

---
+++ tools/corpus_merge.py
@@ -0,0 +1,70 @@
+# Copyright (c) 2023 Asa Katida <github@holomaplefeline.net>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+"""corpus_merge.py"""
+
+from asyncio import run
+from itertools import chain
+from pathlib import Path
+from sys import stderr
+
+from asyncio_cmd import ProcessError
+from corpus_utils import fuzz_star, fuzz_test, sha
+
+SOURCE = Path(__file__).parent.parent.resolve()
+FUZZ = SOURCE / "unit_tests" / "fuzz"
+CORPUS = FUZZ / "corpus"
+CORPUS_ORIGINAL = FUZZ / "corpus_original"
+CRASHES = FUZZ / "crashes"
+
+
+async def main() -> None:
+    if not fuzz_star():
+        raise FileNotFoundError("No fuzz targets built")
+    CORPUS.mkdir(exist_ok=True)
+    CRASHES.mkdir(exist_ok=True)
+    CORPUS.rename(CORPUS_ORIGINAL)
+    CORPUS.mkdir(exist_ok=True)
+    errors = await fuzz_test(
+        "-merge=1",
+        "-reduce_inputs=1",
+        "-shrink=1",
+        CORPUS,
+        CORPUS_ORIGINAL,
+        timeout=3600,
+    )
+    if errors:
+        error = errors.pop()
+        if errors:
+            print("Extra Errors:", *errors, file=stderr, sep="\n")
+        raise error
+    for file in chain.from_iterable(
+        map(SOURCE.rglob, ("crash-*", "leak-*", "timeout-*"))
+    ):
+        file.rename(CRASHES / sha(file))
+
+
+if __name__ == "__main__":
+    try:
+        run(main())
+    except ProcessError as error:
+        error.exit()
+    except KeyboardInterrupt:
+        print("KeyboardInterrupt", file=stderr)
