a4e054466f83df52fa78b197c65ee6643b10809e
Asa Katida <github@holomaplefeline.net>
2023-04-15 17:52:04 -0700

    Fix argument passing to fuzzer.

--- tools/fuzz.py
+++ tools/fuzz.py
@@ -9,20 +9,13 @@ from asyncio_cmd import ProcessError, cmd
 async def fuzz(fuzzer: str, dictionary: str, *dirs: str) -> None:
     await cmd(
         f"./fuzz-{fuzzer}",
-        "-dict",
-        dictionary,
-        "-max_len",
-        16384,
-        "-max_total_time",
-        120,
-        "-print_final_stats",
-        1,
-        "-reduce_inputs",
-        1,
-        "-shrink",
-        1,
-        "-use_value_profile",
-        1,
+        f"-dict={dictionary}",
+        "-max_len=16384",
+        "-max_total_time=120",
+        "-print_final_stats=1",
+        "-reduce_inputs=1",
+        "-shrink=1",
+        "-use_value_profile=1",
         *filter(
             Path.is_dir,  # type: ignore
             chain.from_iterable(map(Path.rglob, map(Path, dirs), repeat("*"))),  # type: ignore
