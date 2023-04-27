834a1b41b7b8fc9c36bb74a714d249005663cf64
Asa Katida <github@holomaplefeline.net>
2023-04-27 16:22:46 -0700

    Log local duplicate branches.

--- tools/git_rebase_all.py
+++ tools/git_rebase_all.py
@@ -22,6 +22,7 @@
 
 from asyncio import run
 from asyncio.subprocess import PIPE
+from itertools import combinations
 from sys import argv, stderr
 
 from asyncio_cmd import ProcessError, cmd, cmd_env, splitlines
@@ -31,21 +32,34 @@ async def git_cmd(*args: object) -> bytes:
     return await cmd("git", *args, err=PIPE, log=False, out=PIPE, timeout=60)
 
 
-async def git_rebase_all(*args: str) -> None:
-    await git_cmd("fetch", "--all", "--prune")
-    remote_branches = await git_cmd("branch", "-r")
-    for remote_branch in splitlines(remote_branches):
-        if "origin/HEAD" in remote_branch or remote_branch.startswith("dependabot/"):
+async def set_upstream(*remote_branches: str) -> None:
+    for remote_branch in remote_branches:
+        if "origin/HEAD" in remote_branch or remote_branch.startswith(
+            "origin/dependabot/"
+        ):
             continue
-        local_name = remote_branch.split("/", maxsplit=1)[-1]
+        local_name = remote_branch.split("/", maxsplit=1)[1]
         try:
             await git_cmd("branch", "--set-upstream-to", remote_branch, local_name)
         except ProcessError:
             await git_cmd("branch", local_name, remote_branch)
-    local_branches = await git_cmd("branch")
-    for local_branch in splitlines(local_branches):
-        if local_branch.startswith("* "):
-            local_branch = local_branch[1:].lstrip()
+
+
+async def git_rebase_all(*args: str) -> None:
+    await git_cmd("fetch", "--all", "--prune")
+    remote_branches = list(splitlines(await git_cmd("branch", "-r")))
+    await set_upstream(*remote_branches)
+    local_branches = list(
+        map(
+            lambda local_branch: (
+                local_branch[1:].lstrip()
+                if local_branch.startswith("* ")
+                else local_branch
+            ),
+            splitlines(await git_cmd("branch")),
+        )
+    )
+    for local_branch in local_branches:
         try:
             await cmd("git", "rebase", "origin/HEAD", local_branch, err=None, out=None)
             continue
@@ -56,6 +70,17 @@ async def git_rebase_all(*args: str) -> None:
         await cmd_env(
             "git", "rebase", "--continue", env={"EDITOR": "true"}, err=None, out=None
         )
+    remote_branches = list(
+        map(
+            lambda remote_branch: remote_branch.split("/", maxsplit=1)[1],
+            remote_branches,
+        )
+    )
+    for left, right in combinations(local_branches, 2):
+        if left in remote_branches and right in remote_branches:
+            continue
+        if not await git_cmd("diff", left, right):
+            print(left, right)
 
 
 if __name__ == "__main__":
