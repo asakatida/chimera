062bbf3a591b94edfafb32cf9e84575a07d037eb
Asa Katida <github@holomaplefeline.net>
2023-05-11 16:04:06 -0700

    Trim extra cbindgen config.

--- oxidation/number/build.rs
+++ oxidation/number/build.rs
@@ -1,6 +1,5 @@
 fn main() {
     println!("cargo:rerun-if-changed=src/");
-    println!("cargo:rerun-if-changed=cbindgen.toml");
     cbindgen::generate(".")
         .unwrap()
         .write_to_file("../number-rust.hpp");
