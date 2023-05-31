99e80388c8c2c60736222823c763e16f26687771
asakatida <asakatida@users.noreply.github.com>
2023-05-31 02:33:36 +0000

    Update docker tag pins to 0.203.0

--- tools/cmake.sh
+++ tools/cmake.sh
@@ -1,6 +1,6 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
-set -ex -o pipefail
+set -ex
 
 build="$1"
 shift
--- tools/modules/builtins.sh
+++ tools/modules/builtins.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
+#!/bin/sh
 
-set -ex -o pipefail
+set -ex
 
 build="$1"
 shift
