edf791e499e1f3604fe9ed83bd66ffab0e7ccad2
Asa Katida <github@holomaplefeline.net>
2023-07-07 14:14:49 -0700

    Rework default `as_completed` thread limits.

--- tools/asyncio_as_completed.py
+++ tools/asyncio_as_completed.py
@@ -5,7 +5,7 @@ from typing import Awaitable, Coroutine, Iterable, Iterator, TypeVar
 
 T = TypeVar("T")
 
-DEFAULT_LIMIT = max(cpu_count() or 0, 4) // 4
+DEFAULT_LIMIT = max(cpu_count() or 0, 1) * 3
 
 
 async def _list(iter: Iterator[Task[T]], cancel: bool) -> list[T]:
@@ -50,7 +50,7 @@ async def _unwrap(task: Awaitable[Task[T]]) -> T:
 
 async def as_completed(
     coroutines: Iterable[Coroutine[object, object, T]],
-    limit: int = max(12, DEFAULT_LIMIT),
+    limit: int = DEFAULT_LIMIT,
     cancel: bool = False,
 ) -> list[T]:
     return await _list(_schedule_tasks(map(create_task, coroutines), limit), cancel)
