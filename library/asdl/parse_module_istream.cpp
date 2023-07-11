591aab8fa947b307bf92fc8152d23b3b6c6a5716
Asa Katida <github@holomaplefeline.net>
2023-07-10 23:42:13 -0700

    Fix tidy nesting.

--- library/asdl/parse_module_istream.cpp
+++ library/asdl/parse_module_istream.cpp
@@ -33,11 +33,9 @@
 #include <string>   // for basic_string
 #include <vector>   // for vector
 
-namespace chimera::library {
-  namespace options {
-    enum class Optimize;
-  } // namespace options
-} // namespace chimera::library
+namespace chimera::library::options {
+  enum class Optimize;
+} // namespace chimera::library::options
 
 namespace chimera::library::asdl {
   Module::Module(const options::Optimize &optimize, std::istream &&input,
