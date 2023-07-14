75a8a27209c04d6ad2b63025b4b954b1499b9556
Asa Katida <github@holomaplefeline.net>
2023-07-13 21:56:53 -0700

    Generate a single file of static fuzz.

--- tools/generate_fuzz.py
+++ tools/generate_fuzz.py
@@ -1,4 +1,5 @@
 from base64 import b64decode
+from itertools import chain
 from json import load
 from pathlib import Path
 from string import printable, whitespace
@@ -24,15 +25,24 @@ def sanitize(data: bytes) -> str:
     )
 
 
-def make_tests(test_cpp: Path, cases: dict[bytes, bytes], prefix: str) -> None:
+def make_tests(test_cpp: Path, tests: dict[str, dict[bytes, bytes]]) -> None:
     content = PREFIX + "\n".join(
-        map(
-            lambda test_name, test_data: (
-                f'TEST_CASE("{prefix} `{test_name}`") {{'
-                f' CHECK_NOTHROW(TestOne("{test_data}")); }}'
-            ),
-            map(lambda name: name.replace("\\", r"\\"), map(sanitize, cases.keys())),
-            map(sanitize, cases.values()),
+        chain.from_iterable(
+            map(
+                lambda prefix, cases: map(
+                    lambda test_name, test_data: (
+                        f'TEST_CASE("fuzz {prefix} `{test_name}`") {{'
+                        f' CHECK_NOTHROW(TestOne("{test_data}")); }}'
+                    ),
+                    map(
+                        lambda name: name.replace("\\", r"\\"),
+                        map(sanitize, cases.keys()),
+                    ),
+                    map(sanitize, cases.values()),
+                ),
+                tests.keys(),
+                tests.values(),
+            )
         )
     )
     if test_cpp.exists() and test_cpp.read_text() == content:
@@ -40,27 +50,26 @@ def make_tests(test_cpp: Path, cases: dict[bytes, bytes], prefix: str) -> None:
     test_cpp.write_text(content)
 
 
-def main(output_dir: str) -> None:
+def main(output: str) -> None:
     with Path(
         "external/big-list-of-naughty-strings/blns.base64.json"
     ).open() as istream:
         blns_base64 = load(istream)
-    make_tests(
-        Path(output_dir) / "fuzz-base64.cpp",
-        dict(zip(map(str.encode, blns_base64), map(str.encode, blns_base64))),
-        "fuzz big list of nasty strings base64",
-    )
-    make_tests(
-        Path(output_dir) / "fuzz-blns.cpp",
-        dict(zip(map(b64decode, blns_base64), map(b64decode, blns_base64))),
-        "fuzz big list of nasty strings",
-    )
     with Path("unit_tests/fuzz/cases.json").open() as istream:
         cases = load(istream)
     make_tests(
-        Path(output_dir) / "fuzz-cases.cpp",
-        dict(zip(map(str.encode, cases.keys()), map(str.encode, cases.values()))),
-        "fuzz discovered cases",
+        Path(output),
+        {
+            "big list of nasty strings base64": dict(
+                zip(map(str.encode, blns_base64), map(str.encode, blns_base64))
+            ),
+            "big list of nasty strings": dict(
+                zip(map(b64decode, blns_base64), map(b64decode, blns_base64))
+            ),
+            "discovered cases": dict(
+                zip(map(str.encode, cases.keys()), map(str.encode, cases.values()))
+            ),
+        },
     )
 
 
