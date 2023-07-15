ba03ebbba462d294433168794d05eda5f84952dc
Asa Katida <github@holomaplefeline.net>
2023-07-15 07:45:24 -0700

    Allow for alternate virtual environment path.

--- tools/venv.py
+++ tools/venv.py
@@ -1,24 +1,24 @@
 from pathlib import Path
 from subprocess import run
-from sys import executable
+from sys import argv, executable
 
 from chimera_utils import rmdir
 
 
-def pip(*args: str, env: dict[str, str]) -> None:
-    run(["env/bin/pip", *args], env=env)
+def pip(venv: str, *args: str, env: dict[str, str]) -> None:
+    run([f"{venv}/bin/pip", *args], env=env)
 
 
-def main() -> None:
-    rmdir(Path("env"))
+def main(venv: str = "env") -> None:
+    rmdir(Path(venv))
     env: dict[str, str] = {}
-    run([executable, "-m", "venv", "env"], env=env)
-    pip("install", "-r", "requirements.core.txt", env=env)
-    pip("install", "-r", "requirements.txt", env=env)
+    run([executable, "-m", "venv", venv], env=env)
+    pip(venv, "install", "-r", "requirements.core.txt", env=env)
+    pip(venv, "install", "-r", "requirements.txt", env=env)
 
 
 if __name__ == "__main__":
     try:
-        main()
+        main(*argv[1:])
     except KeyboardInterrupt:
         print("\nExiting...")
