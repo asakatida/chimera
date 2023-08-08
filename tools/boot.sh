3badb14ec64c374cfeed13dce78533d037aca5da
Asa Katida <github@holomaplefeline.net>
2023-08-07 20:53:22 -0700

    Update usage of `CXXFLAGS` to be simpler.

--- tools/boot.sh
+++ tools/boot.sh
@@ -29,17 +29,14 @@ cmakelint
 
 chimera_path="${PWD}/stdlib:$(python3 tools/chimera_path.py)"
 
-export CC="${CC:-clang}"
-export CXX="${CXX:-clang++}" CXXFLAGS="${CXXFLAGS} -DCHIMERA_PATH=${chimera_path}"
+export CXXFLAGS="${CXXFLAGS} -DCHIMERA_PATH=${chimera_path}"
 
 export LLVM_PROFILE_FILE="${PWD}/build/coverage/llvm-profile.%c%p.profraw"
 
-env \
-  CMAKE_BUILD_TYPE=Debug \
-  tools/cmake.sh "${PWD}/build/debug" -Wdev -Werror=dev
+tools/cmake.sh "${PWD}/build/debug" -Wdev -Werror=dev
 
 env \
-  CMAKE_BUILD_TYPE="${CMAKE_BUILD_TYPE:-MinSizeRel}" \
+  CMAKE_BUILD_TYPE=MinSizeRel \
   CXXFLAGS="${CXXFLAGS} -DNDEBUG" \
   tools/cmake.sh "${PWD}/build/release"
 
