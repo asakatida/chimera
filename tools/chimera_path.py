dd2f8de9d0370308f31d3dfbe171c66920b1d0a5
asakatida <asakatida@users.noreply.github.com>
2023-11-13 14:27:07 +0000

    Update requirements.txt.

--- tools/chimera_path.py
+++ tools/chimera_path.py
@@ -3,6 +3,6 @@ from sys import path
 
 tools = Path(__file__).parent.resolve()
 
-paths: list[Path] = list(map(Path.resolve, map(Path, path)))  # type: ignore
+paths: list[Path] = list(map(Path.resolve, map(Path, path)))
 paths.remove(tools)
 print(*paths, sep=":")
